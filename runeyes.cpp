77c951a87bfcd32d15f1c5b56b232c25a47c7ab0
Bernd Stramm <bernd.stramm@gmail.com>
2009-12-04 18:27:21 -0500

    remove debug

--- runeyes.cpp
+++ runeyes.cpp
@@ -23,15 +23,12 @@ main (int argc, char *argv[])
   QTranslator  trans;
   QString locale = QLocale::system().name();
   QString filename = QString(":eyes_") + locale ;
-  qDebug() << " language file " << filename;
   bool ok = trans.load (filename);
-  qDebug ()  << " ok is " << ok;
   App.installTranslator(&trans);
 
   eyes::eyes TheEyes(&App);
 
   TheEyes.SetDelay(100);
-qDebug() << " we are in " << locale;
   TheEyes.show();
   App.exec();
 
